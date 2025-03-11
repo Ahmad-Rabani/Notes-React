704fb614c7db5551d7bd6ee286d23abfa3e4566c
Ahmad-Rabani <ahmadalirabani07@gmail.com>
2025-03-11 16:51:47 +0500

    Finalized

--- src/Components/card/_redux/CardSlice.js
+++ src/Components/card/_redux/CardSlice.js
@@ -1,15 +1,14 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { db } from "../../../firebase";
 import { doc, setDoc, addDoc, collection } from "firebase/firestore";
-import { fetchNotes } from "../../main/_redux/MainSlice"; // Import fetchNotes to refetch data after saving
+import { fetchNotes } from "../../main/_redux/MainSlice";
 
-// Thunk to save a new or existing note
 export const saveNote = createAsyncThunk(
   "notes/saveNote",
   async ({ updatingData, userUid, noteData }, { dispatch }) => {
     const noteRef = updatingData
-      ? doc(db, "newData", updatingData.id) // Updating existing note
-      : doc(collection(db, "newData")); // Creating new note
+      ? doc(db, "newData", updatingData.id)
+      : doc(collection(db, "newData"));
 
     await setDoc(noteRef, {
       id: updatingData ? updatingData.id : noteRef.id,
